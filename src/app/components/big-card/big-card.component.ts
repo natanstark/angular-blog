028f9ea14b741af7394cb788bb8df5ce5b71dd73
natanstark <natanvstark@icloud.com>
2023-10-12 23:23:45 -0300

    content pages created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,11 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
   @Input()
-  imageCover: string= "";
+  imageCover:string= "";
   @Input()
-  cardTitle: string= "";
+  cardTitle:string= "";
   @Input()
-  cardDescription: string= "";
+  cardDescription:string= "";
+  @Input()
+  Id:string="0";
 
   constructor() { }
 
